2ec3615245b293ed6a8bc3b6cb284da524535fff
Sebastien Dehesdin <seb@bleepsandblops.com>
2016-04-05 23:51:13 +0200

    removed concole headers

--- controllers/scannercamera.js
+++ controllers/scannercamera.js
@@ -47,8 +47,8 @@ exports.doScan = function(req, res, next) {
 
     var download = function(uri, filename, callback) {
         request.head(uri, function(err, res, body) {
-            console.log('content-type:', res.headers['content-type']);
-            console.log('content-length:', res.headers['content-length']);
+            //console.log('content-type:', res.headers['content-type']);
+            //console.log('content-length:', res.headers['content-length']);
             request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
 
 
